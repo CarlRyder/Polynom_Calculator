2b474e64b3947a3f0bcfe0203c6dedf9d10bfa15
CarlRyder <yasendov@gmail.com>
2024-04-28 02:36:14 +0300

    [feature] Добавлены комментарии

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
 #include "poly.h"
 #include "parser.tab.h"
 
+FILE *streamIn;
+
+#define yyin streamIn
+#define DEFAULT_ERROR -1
+
 int main()
 {
+    streamIn = fopen("poly.txt", "r");
+    if (streamIn == NULL)
+    {
+        printf("[-] File with polynoms doesn't exist\n");
+        return DEFAULT_ERROR;
+    }
     yyparse();
-    return 0;
+    fclose(streamIn);
 }
